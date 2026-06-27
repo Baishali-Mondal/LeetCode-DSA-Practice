0a3493657be4bd6f26e8e4f05acc01f845b4ac75
Vaishali <missbaishali11@gmail.com>
2026-06-27 19:30:16 +0530

    Modified the Hashmap approach

--- PATTERNS/169. Majority Element/Majority Element.java
+++ PATTERNS/169. Majority Element/Majority Element.java
@@ -4,27 +4,38 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
 
-        //Storing the value in the map
-        for(int i=0; i<n; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i])+1);
+//        //Storing the value in the map
+//        for(int i=0; i<n; i++){
+//            if(map.containsKey(nums[i])){
+//                map.put(nums[i], map.get(nums[i])+1);
+//            }
+//            else{
+//                map.put(nums[i],1);
+//            }
+//        }
+//
+//        //Map Traverse
+//        for(int key : map.keySet()){ //keyset() method returns all the key contained within a map
+//            if(map.get(key) > n/2)
+//            return key;
+//        }
+        //Storing the frequency as well look for a freq > n/2
+        for(int num :nums){
+            if(map.containsKey(num)){
+                map.put(num, map.get(num)+1);
+            }else{
+                map.put(num, 1);
             }
-            else{
-                map.put(nums[i],1);
+            if(map.get(num) > n/2){
+                return num;
             }
-        }
-
-        //Map Traverse
-        for(int key : map.keySet()){ //keyset() method returns all the key contained within a map
-            if(map.get(key) > n/2)
-            return key;
         }
         return -1;
     }
 }
 
 //// OPTIMIZED - USING BOYER MOORE VOTING ALGO (Choose a candidate and vote for/against it)  
-//--> TC-O(1) SC-O(1)
+//--> TC-O(N) SC-O(1)
 class Solution {
     public int majorityElement(int[] nums) {
         int count = 0;
