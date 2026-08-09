1525d7f9b2e995cf7f3b021f24a72d94a068b68c
Vaishali <missbaishali11@gmail.com>
2026-08-09 13:42:55 +0530

    Added Subset Sum Problem-GFG

---
+++ PATTERNS/Recursion/Subset Sum Problem/Subset Sum Problem.java
@@ -0,0 +1,24 @@
+//USING RECURSION
+// Time Complexity: O(2^n) — every element has 2 choices(pick or not pick).
+// Auxiliary Space: O(n)
+
+class Solution {
+    static boolean solve(int[] arr, int target, int ind){
+        
+        //BASE CASE
+        if(target == 0){
+            return true;
+        }
+        
+        if(ind == arr.length || target < 0){
+            return false;
+        }
+                //PICK or //NOT PICK
+        return solve(arr, target-arr[ind], ind+1) || solve(arr, target, ind+1);
+    }
+
+    static boolean isSubsetSum(int arr[], int sum) {
+
+        return solve(arr, sum, 0);
+    }
+}
