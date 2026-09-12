5402f75f2c8d69956f6bd0881f718f197ddfd1cb
Vaishali <missbaishali11@gmail.com>
2026-09-12 13:29:09 +0530

    Added Next Greater Element GFG

---
+++ PATTERNS/Stack/Next Greater Element/Next Greater Element.java
@@ -0,0 +1,30 @@
+// TC - O(n)
+// SC - O(n)
+
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int n = arr.length;
+        
+        for(int i=n-1; i>=0; i--){ //traversing the array from right to optimize O(n^2) and storing next greater while traversing
+                                                                       
+            while(!st.isEmpty() && st.peek() <= arr[i]){ // top < element
+                st.pop();
+            }
+            
+            if(st.isEmpty()){ // stack is empty
+                list.add(-1);
+            }
+            else if(st.size() > 0 && st.peek() > arr[i]){ // top > elemnt
+                list.add(st.peek());
+            }
+            
+            st.push(arr[i]);
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
